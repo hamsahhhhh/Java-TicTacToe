a74146135e3dc659ac411efb89d275dee23429d4
hamsa mohamed <hamsabader0@gmail.com>
2021-04-09 13:44:55 +0300

    delete java class

---
+++ src/ai.kt
@@ -0,0 +1,8 @@
+class ai {
+    class AI { //NOT YET USED
+        //easy - will make losing moves
+        //medium - will try to make a tictactoe
+        //hard - will block other player's tictactoe
+        //stupid - will make random moves (easiest to code)
+    }
+}
